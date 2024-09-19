2ca22236f2ad033913bbe821b572f492bed3049c
ScottGreenhalgh <scottgreenhalgh62@me.com>
2024-09-19 14:15:00 +0100

    added useState for cookie count

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "../styles/App.css";
 import Header from "./Header";
 import LeftSidebar from "./LeftSidebar";
@@ -5,10 +6,13 @@ import Main from "./Main";
 import RightSidebar from "./RightSidebar";
 
 function App() {
+  let [count, setCount] = useState(0);
+  let [cps, setCps] = useState(0);
+
   return (
     <div className="container" aria-live="polite">
       <Header />
-      <LeftSidebar />
+      <LeftSidebar count={count} setCount={setCount} cps={cps} />
       <Main />
       <RightSidebar />
     </div>
--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -1,19 +1,42 @@
+import { useEffect } from "react";
 import "../styles/LeftSidebar.css";
+import formatter from "../utils/formatter";
 
-export default function LeftSidebar() {
+export default function LeftSidebar({ count, setCount, cps }) {
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  useEffect(() => {
+    console.log("useEffect running");
+    const myInterval = setInterval(() => {
+      console.log("This is running interval");
+      setCount((cookies) => cookies + 1 * cps);
+    }, 1000);
+
+    return () => {
+      clearInterval(myInterval);
+    };
+  }, [cps, setCount]);
+
   return (
     <div id="left-sidebar" className="left-sidebar">
       <h2 className="left-sidebar-text">Bakery</h2>
       <p className="left-sidebar-text">Cookies:</p>
-      <p className="left-sidebar-text" id="cookieCount"></p>
+      <p className="left-sidebar-text" id="cookieCount">
+        {formatter(count)}
+      </p>
 
       <p className="left-sidebar-text">Per Second:</p>
-      <p className="left-sidebar-text" id="cookiesPerSecond"></p>
+      <p className="left-sidebar-text" id="cookiesPerSecond">
+        {formatter(cps)}
+      </p>
       <img
         src="./src/assets/cookie.webp"
         alt="big cookie"
         id="cookie-image"
         aria-label="Clickable big cookie image"
+        onClick={handleClick}
       />
     </div>
   );
